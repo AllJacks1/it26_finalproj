7867bbd5a55215c47cc35a3472bfde2bbf85aae5
ジャックスオール <101043495+AllJacks@users.noreply.github.com>
2023-04-05 16:12:49 +0800

    s

--- patient.php
+++ patient.php
@@ -1,88 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
-      integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-<<<<<<<< HEAD:doctor.php
-    <link rel="stylesheet" href="doctor.css?v=<?php echo time(); ?>">
-========
-    <link rel="stylesheet" href="patient.css">
->>>>>>>> fb6055acfbc7600afbef5429eb858dc68a573542:patient.html
-    <title>Document</title>
-  </head>
-  <body>
-    <div class="container">
-      <div class="navigation">
-        <ul>
-          <li>
-            <a href="#">
-              <span class="logo">
-                <img src="logo.ico" alt="" />
-              </span>
-              <span class="logo-name">St. Kerby Hospital</span>
-            </a>
-          </li>
-          <li id="dashboard">
-<<<<<<<< HEAD:patient.php
-            <a href="adminMain.php">
-========
-<<<<<<<< HEAD:doctor.php
-            <a href="adminMain.php">
-========
-            <a href="adminMain.html">
->>>>>>>> fb6055acfbc7600afbef5429eb858dc68a573542:patient.html
->>>>>>>> f61059ec7f631a1cf1d239e193d3902bbcd0d36b:patient.html
-              <span class="icon">
-                <i class="fa-solid fa-house"></i>
-              </span>
-              <span class="title">Dashboard</span>
-            </a>
-          </li>
-          <li>
-<<<<<<<< HEAD:patient.php
-            <a href="patient.php">
-========
-<<<<<<<< HEAD:doctor.php
-            <a href="patient.php">
-========
-            <a href="patient.html">
->>>>>>>> fb6055acfbc7600afbef5429eb858dc68a573542:patient.html
->>>>>>>> f61059ec7f631a1cf1d239e193d3902bbcd0d36b:patient.html
-              <span class="icon">
-                <i class="fa-solid fa-hospital-user"></i>
-              </span>
-              <span class="title">Patient</span>
-            </a>
-          </li>
-          <li>
-<<<<<<<< HEAD:patient.php
-            <a href="doctor.php">
-========
-<<<<<<<< HEAD:doctor.php
-            <a href="doctor.php">
-========
-            <a href="doctor.html">
->>>>>>>> fb6055acfbc7600afbef5429eb858dc68a573542:patient.html
->>>>>>>> f61059ec7f631a1cf1d239e193d3902bbcd0d36b:patient.html
-              <span class="icon">
-                <i class="fa-solid fa-user-doctor"></i>
-              </span>
-              <span class="title">Doctor</span>
-            </a>
-          </li>
-          <li>
-<<<<<<<< HEAD:patient.php
-            <a href="addRecords.php">
-========
-<<<<<<<< HEAD:doctor.php
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="patient.css?v=<?php echo time(); ?>">
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="navigation">
+      <ul>
+        <li>
+          <a href="#">
+            <span class="logo">
+              <img src="logo.ico" alt="" />
+            </span>
+            <span class="logo-name">St. Kerby Hospital</span>
+          </a>
+        </li>
+
+        <li id="dashboard">
+          <a href="adminMain.php">
+            <span class="icon">
+              <i class="fa-solid fa-house"></i>
+            </span>
+            <span class="title">Dashboard</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="patient.php">
+            <span class="icon">
+              <i class="fa-solid fa-hospital-user"></i>
+            </span>
+            <span class="title">Patient</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="doctor.php">
+            <span class="icon">
+              <i class="fa-solid fa-user-doctor"></i>
+            </span>
+            <span class="title">Doctor</span>
+          </a>
+        </li>
+
+        <li>
           <a href="medicine.php">
             <span class="icon">
               <i class="fa-solid fa-tablets"></i>
@@ -91,174 +59,93 @@
           </a>
         </li>
 
-          <li>
-            <a href="addRecords.php">
-========
-            <a href="addRecords.html">
->>>>>>>> fb6055acfbc7600afbef5429eb858dc68a573542:patient.html
->>>>>>>> f61059ec7f631a1cf1d239e193d3902bbcd0d36b:patient.html
-              <span class="icon">
-                <i class="fa-solid fa-user-plus"></i>
-              </span>
-              <span class="title">Add records</span>
-            </a>
-          </li>
+        <li>
+          <a href="addRecords.php">
+            <span class="icon">
+              <i class="fa-solid fa-user-plus"></i>
+            </span>
+            <span class="title">Add records</span>
+          </a>
+        </li>
 
-          <li>
-<<<<<<<< HEAD:doctor.php
-            <a href="manageRecords.php">
-========
-            <a href="#">
->>>>>>>> fb6055acfbc7600afbef5429eb858dc68a573542:patient.html
-              <span class="icon">
-                <i class="fa-solid fa-pen-to-square"></i>
-              </span>
-              <span class="title">Manage records</span>
-            </a>
-          </li>
-        </ul>
+        <li>
+          <a href="manageRecords.php">
+            <span class="icon">
+              <i class="fa-solid fa-pen-to-square"></i>
+            </span>
+            <span class="title">Manage records</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="main">
+    <div class="topbar">
+      <div class="toggle">
+        <i class="fa-solid fa-bars"></i>
+      </div>
+
+      <div class="search">
+        <label>
+          <input type="text" placeholder="Search Patient">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </label>
+
+
       </div>
     </div>
-    <div class="main">
-        <div class="topbar">
-            <div class="toggle">
-              <i class="fa-solid fa-bars"></i>
-            </div>
 
-            <div class="search">
-                <label>
-<<<<<<<< HEAD:doctor.php
-                    <input type="text" placeholder="Search Doctor">
-========
-                    <input type="text" placeholder="Search Patient ID">
->>>>>>>> fb6055acfbc7600afbef5429eb858dc68a573542:patient.html
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </label>
-            </div>
+    <div class="details">
+      <div class="patient">
+        <div class="cardHeader">
+          <h2>Patient's Information </h2>
+          <a href="#" class="btn">View All</a>
         </div>
-        <div class="details">
-<<<<<<<< HEAD:doctor.php
-            <div class="doctor">
-                <div class="cardHeader">
-                    <h2>Doctor's Information</h2>
-                    <a href="#" class="btn">View All</a>
-                </div>
-                <table class="table table-striped table-bordered table-hover">
-                  <thead>
-                    <th>Doctor ID</th>
-                    <th>Fullname</th>
-                    <th>Gender</th>
-                    <th>Specialization</th>
-                  </thead>
-                  <tbody>
-                  <?php
-                    include("database.php");
-                    include("functions.php");
-                    
-                    $query=mysqli_query($con,"select * from `doctor`");
-                    while($row=mysqli_fetch_array($query)){
-                      ?>
-                      <tr>
-                        <td><?php echo ucwords($row['doctorID']); ?></td>
-                        <td><?php echo ucwords($row['fullname']); ?></td>
-                        <td><?php echo $row['gender']; ?></td>
-                        <td><?php echo ucwords($row['specialization']); ?></td>
-                      </tr>
-                      <?php
-                    }
-                  ?>
-                  </tbody>
-========
-            <div class="patient">
-                <div class="cardHeader">
-                    <h2>Patient's Information   </h2>
-                    <a href="#" class="btn">View All</a>
-                </div>
-<<<<<<<< HEAD:patient.php
-                <table class="table table-striped table-bordered table-hover">
-                  <thead>
-                    <th>Patient ID</th>
-                    <th>Fullname</th>
-                    <th>Age</th>
-                    <th>Gender</th>
-                  </thead>
-                  <tbody>
-                  <?php
-                    include("database.php");
-                    include("functions.php");
-                    
-                    $query=mysqli_query($con,"SELECT patientID, CONCAT(fn, ' ',  mi, '. ', ln) AS 'fullname', age, gender FROM `patient` WHERE 1");
-                    while($row=mysqli_fetch_array($query)){
-                      ?>
-                      <tr>
-                        <td><?php echo ucwords($row['patientID']); ?></td>
-                        <td><?php echo ucwords($row['fullname']); ?></td>
-                        <td><?php echo $row['age']; ?></td>
-                        <td><?php echo ucwords($row['gender']); ?></td>
-                      </tr>
-                      <?php
-                    }
-                  ?>
-                  </tbody>
-========
-            
-    
-                <table>
-                    <thead>
-                        <tr>
-                            <td>PatientID</td>
-                            <td>Last name</td>
-                            <td>First Name</td>
-                            <td>Middle Name</td>
-                            <td>Gender</td>
-                        </tr>
-                    </thead>
+        <table>
+          <thead>
+            <tr>
+              <th>PatientID</th>
+              <th>Last name</th>
+              <th>First Name</th>
+              <th>Middle Initial</th>
+              <th>Age</th>
+              <th>Gender</th>
 
-                    <tbody>
-                        <tr>
-                            <td>PatientID</td>
-                            <td>Last name</td>
-                            <td>First Name</td>
-                            <td>Middle Name</td>
-                            <td>Gender</td>
-                        </tr>
-                    </tbody>
+            </tr>
+          </thead>
 
-                    <tbody>
-                        <tr>
-                            <td>PatientID</td>
-                            <td>Last name</td>
-                            <td>First Name</td>
-                            <td>Middle Name</td>
-                            <td>Gender</td>
-                        </tr>
-                    </tbody>
+          <tbody>
+            <?php
 
-                    <tbody>
-                        <tr>
-                            <td>PatientID</td>
-                            <td>Last name</td>
-                            <td>First Name</td>
-                            <td>Middle Name</td>
-                            <td>Gender</td>
-                        </tr>
-                    </tbody>
+            include("database.php");
+            include("functions.php");
 
-                    <tbody>
-                        <tr>
-                            <td>PatientID</td>
-                            <td>Last name</td>
-                            <td>First Name</td>
-                            <td>Middle Name</td>
-                            <td>Gender</td>
-                        </tr>
-                    </tbody>
->>>>>>>> fb6055acfbc7600afbef5429eb858dc68a573542:patient.html
->>>>>>>> f61059ec7f631a1cf1d239e193d3902bbcd0d36b:patient.html
-                </table>
-            </div>
+            $query = mysqli_query($con, "select * from `patient`");
+            while ($row = mysqli_fetch_array($query)) {
+            ?>
+              <tr>
+                <td><?php echo ucwords($row['patientID']); ?></td>
+                <td><?php echo ucwords($row['ln']); ?></td>
+                <td><?php echo $row['fn']; ?></td>
+                <td><?php echo ucwords($row['mi']); ?></td>
+                <td><?php echo $row['age']; ?></td>
+                <td><?php echo $row['gender']; ?></td>
+              </tr>
+            <?php
+            }
+            ?>
+          </tbody>
+        </table>
       </div>
     </div>
-    <script src="adminMain.js"></script>
-  </body>
+
+
+
+
+  </div>
+
+  <script src="adminMain.js"></script>
+</body>
+
 </html>
